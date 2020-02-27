35e55da2cd87784f78b5d2644a991dc0995ba70c
Rudy Sarrazin <wodcycle@gmail.com>
2020-02-27 17:31:41 +0100

    minor

--- source/function.py
+++ source/function.py
@@ -8,7 +8,7 @@ def reward_function(params):
     prediction_weight = 0.7
     speed_max = 4
     dist_virtual_waypoints_ratio = 1/2
-    waypoint_view_min = 3
+    waypoint_view_min = 5
     waypoint_view_max = 15
 
     x = params['x']
